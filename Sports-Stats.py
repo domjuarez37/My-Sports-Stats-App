eecb6134476b7f4783aedd5b165962f960efe292
Dominic Juarez <dojuarez@seu.edu>
2026-04-12 12:50:59 -0400

    Added a class to hold baseball stats. also tested the class with a practice run.

---
+++ Sports-Stats.py
@@ -0,0 +1,15 @@
+class Baseball:
+    def __init__(self, name, Homeruns, RBIs, SBs):
+        self.name= name
+        self.Homeruns= Homeruns
+        self.RBIs= RBIs
+        self.SBs= SBs
+    def __str__(self):
+        return f"{self.name} has {self.Homeruns} home runs, {self.RBIs} RBIs, and {self.SBs} stolen bases "
+
+
+
+
+
+player= Baseball("Gunnar Henderson",92,272,64)
+print(player)         
