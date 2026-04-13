67dbf505e3a5231f761610cee195c80c10d80ac1
Dominic Juarez <dojuarez@seu.edu>
2026-04-13 12:45:32 -0400

    added organization to my dictionary. Imported my classes from sports_stats

--- add_player.py
+++ add_player.py
@@ -1,3 +1,16 @@
 from sports_stats import MLB, NFL_O, NFL_D, NBA
 
 players= {}
+
+def add_player():
+  sport = input("Choose sport (MLB, NFL_O, NFL_D, NBA): ")  
+    
+  name= input("Name of player: ")
+
+#add_player()
+
+
+
+print(players.items)
+
+   
