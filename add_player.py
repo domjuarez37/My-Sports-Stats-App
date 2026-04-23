8214f3de223806873e602e934f0e1508c6ea0421
Dominic Juarez <dojuarez@seu.edu>
2026-04-23 13:28:21 -0400

    created a loop to add players. wrote it to a csv.

--- add_player.py
+++ add_player.py
@@ -1,17 +1,38 @@
-from sports_stats import MLB, NFL_O, NFL_D, NBA
-
-players= {}
-
-def add_player():
-  sport = input("Choose sport (MLB, NFL_O, NFL_D, NBA): ")  
-  name= input("Name of player: ")
-  players[name]= sport
-while True:
-    add_player()
-    another = input("Add another player? (y/n): ")
-    if another == "n":
-        break
+from sports_stats import All_Players
 
 
-print(players)
+def player_database():
+    database=[]
+    # Used AI to help with creation of the file
+    with open("database.csv","a+") as file:
+      file.seek(0)
+      for line in file:
+          name, sport = line.strip().split(",")
+          database.append(All_Players(name, sport))
+    return database
+                  
+
+
+
+
+def add_player(database):
+
+    name = input("Name: ")
+    sport = input("Sport: ")
+
+    player= All_Players(name,sport)
+    database.append(player)
+
+
+    with open("database.csv", "a") as file:
+        file.write(f"{name},{sport}\n")
+
+    print(f"Entry added: {name}:")
+
+db = player_database()
    
+while True:
+      add_player(db)
+      another = input("Add another player? (y/n): ")
+      if another == "n":
+        break
